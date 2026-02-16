7ac8ed79389c779a19f054fe383278d9f37618e6
SureshThapa <sambridthapa6@gmail.com>
2026-02-16 14:19:31 +0545

    bulkorder to bulkOrder import model in controller

--- api/index.js
+++ api/index.js
@@ -75,5 +75,11 @@ app.use(errorHandler);
 connectDB().catch((err) => {
   console.error("DB connection error:", err);
 });
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
 
 export default app;
